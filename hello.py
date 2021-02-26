f2f44324c986d0ec85b0c909fcdc92aee26c3950
staselko <staselya2002@mail.ru>
2021-02-26 18:41:55 +0300

    added dockerfile and upgraded python script

--- hello.py
+++ hello.py
@@ -1,5 +1,23 @@
-number = input('Input а number')
-if int(number) == 5845555:
-    print('Oh, its my phone number')
-else :
-    print('Nice')
+nameList = []
+def app():
+	print('\nWhat u want to do?')
+	print('1.Add Name \n2.See Name list \n3.Exit')
+	counter = int(input())
+	if counter == 1 : 
+		print('Enter a new name')
+		newName = str(input())
+		nameList.append(newName)
+		return app()
+		
+	elif counter == 2 :
+		if len(nameList) == 0:
+			print('\nName list is empty')	
+		print('\nYour Name List: \n')	
+		print('\n'.join(nameList))	
+		return app()
+	elif counter == 3 :
+    		return 0	
+	else :
+		print('U entered a wrong value')			
+		return app()
+app()
